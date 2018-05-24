7d49ad476de424fd32f72139f546f0ea2ae2630c
Andrew Doser <andrew.doser@oit.edu>
2018-05-23 23:10:51 -0700

    Add superficial stuff
    Recursive function not working correctly.
    Keeps Crashing on second check

--- app/src/main/java/com/example/andrewdoser/connectmydudes/MainActivity.java
+++ app/src/main/java/com/example/andrewdoser/connectmydudes/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button[][] buttons = new Button[11][11];
     //public Connector[][] buttons = new Connector[11][11];
     public boolean[][] ifTouch = new boolean[11][11];
-
+    public String[][] Cont = new String[11][11];
 
     private boolean player1turn = true;
     private int roundCount;
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public int traverse = 0;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -38,30 +39,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         textViewPlayer1 = findViewById(R.id.text_view_p1);
         textViewPlayer2 = findViewById(R.id.text_view_p2);
+
         int tx, ty;
         tx = 0;
         ty = 0;
+
+
 
         String temp = "";
         for (int i = 0; i < 11; i++)
         {
             for(int j = 0; j < 11; j++)
             {
+
                 temp = "button_" + i + "_" + j;
                 int resID = getResources().getIdentifier(temp,"id",getPackageName());
                 buttons[i][j] = findViewById(resID);
                 buttons[i][j].setOnClickListener(this);
                 ifTouch[i][j] = false;
+
+            }
+        }
+        for (int i = 0; i < 11; i+=2)
+        {
+            for(int j = 1; j < 11; j+=2)
+            {
+               buttons[j][i].setText("X");
+               buttons[j][i].setBackgroundColor(Color.GREEN);
+            }
+        }
+        for (int i = 1; i < 11; i+=2)
+        {
+            for(int j = 0; j < 11; j+=2)
+            {
+                buttons[j][i].setText("O");
+                buttons[j][i].setBackgroundColor(Color.BLUE);
             }
         }
 
-        Button button_Reset = findViewById(R.id.button_reset);
+
+        final Button button_Reset = findViewById(R.id.button_reset);
         button_Reset.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-
+                buttReset();
             }
         });
 
@@ -69,10 +92,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    public void PlayerWon(char player)
+    public void buttReset()
+    {
+        for (int i = 0; i < 11; i++)
+        {
+            for(int j = 0; j < 11; j++)
+            {
+                buttons[j][i].setText("");
+                buttons[j][i].setBackgroundResource(android.R.drawable.btn_default);
+            }
+        }
+        for (int i = 0; i < 11; i+=2)
+        {
+            for(int j = 1; j < 11; j+=2)
+            {
+                buttons[j][i].setText("X");
+                buttons[j][i].setBackgroundColor(Color.GREEN);
+            }
+        }
+        for (int i = 1; i < 11; i+=2)
+        {
+            for(int j = 0; j < 11; j+=2)
+            {
+                buttons[j][i].setText("O");
+                buttons[j][i].setBackgroundColor(Color.BLUE);
+            }
+        }
+    }
+
+    public void PlayerWon(String player)
     {
         Context context = getApplicationContext();
-        if (player == 'X')
+        if (player.equals("X"))
         {
             CharSequence warning = "Player 1 won!";
             int duration = Toast.LENGTH_LONG;
@@ -106,29 +157,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(player1turn)
         {
             ((Button) v).setText("X");
+            ((Button) v).setBackgroundColor(Color.GREEN);
+            player1turn = false;
         }
         else
         {
             ((Button) v).setText("O");
+            ((Button) v).setBackgroundColor(Color.BLUE);
+            player1turn = true;
         }
 
         roundCount++;
 
 
-        for (int i = 0; i < 11; i++)
+      /*  for (int i = 0; i < 11; i++)
         {
             for (int j = 0; j < 11; j++)
             {
                 if (player1turn)
                 {
-                    if(checkForWin('X',i,j))
+                    if(checkForWin("X",i,j))
                     {
                         player1points++;
                     }
                 }
                 else
                 {
-                    if(checkForWin('O',i,j))
+                    if(checkForWin("O",i,j))
                     {
                         player2points++;
                     }
@@ -136,12 +191,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
             }
-        }
+        }*/
 
 
     }
+    public void RefillCont()
+    {
+        for(int i = 0; i <11; i++)
+        {
+            for (int j = 0; j < 11; j++)
+            {
+                Cont[i][j] = buttons[i][j].getText().toString();
+                ifTouch[i][j]=false;
+            }
+        }
+    }
 
-    private boolean checkForWin(char tc, int tx, int ty) {
+
+    /*private boolean checkForWin(String tc, int tx, int ty) {
 
         if(Checkstart == false)
         {
@@ -150,22 +217,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Checkstart = true;
 
         }
-
+        RefillCont();
 
         if(tx < 10)
         {
-            if((buttons[tx][ty].getText().toString().equals(tc)) && (buttons[tx+1][ty].getText().toString().equals(tc)) && ifTouch[tx+1][ty] == false)
+            if(Cont[tx][ty].equals(tc) && Cont[tx+1][ty].equals(tc) && !ifTouch[tx + 1][ty])
             {
                 ifTouch[tx][ty] = true;
                 traverse++;
-                return checkForWin(tc,tx +1, ty);
+                return checkForWin(tc,tx+1, ty);
             }
 
         }
 
         if(tx > 0)
         {
-            if ((buttons[tx][ty].getText().toString().equals(tc)) && (buttons[tx - 1][ty].getText().toString().equals(tc)) && !ifTouch[tx - 1][ty]) {
+            if (Cont[tx][ty].equals(tc) && Cont[tx-1][ty].equals(tc) && !ifTouch[tx - 1][ty]) {
                 ifTouch[tx][ty] = true;
                 traverse++;
                 return checkForWin(tc,tx - 1, ty);
@@ -174,7 +241,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         if(ty < 10)
         {
-            if ((buttons[tx][ty].getText().toString().equals(tc)) && (buttons[tx][ty + 1].getText().toString().equals(tc)) && !ifTouch[tx][ty + 1]) {
+            if (Cont[tx][ty].equals(tc) && Cont[tx][ty+1].equals(tc) && !ifTouch[tx][ty + 1]) {
                 ifTouch[tx][ty] = true;
                 traverse++;
                 return checkForWin(tc,tx, ty + 1);
@@ -182,7 +249,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         if(ty > 0)
         {
-            if ((buttons[tx][ty].getText().toString().equals(tc)) && (buttons[tx][ty - 1].getText().toString().equals(tc)) && !ifTouch[tx][ty - 1]) {
+            if (Cont[tx][ty].equals(tc) && Cont[tx][ty-1].equals(tc) && !ifTouch[tx][ty - 1]) {
                 ifTouch[tx][ty] = true;
                 traverse++;
                 return checkForWin(tc,tx, ty - 1);
@@ -190,13 +257,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         Checkstart = false;
-        for (int i =0; i < 11; i++)
-        {
-            for (int j = 0; j < 1; j++)
-            {
-                ifTouch[i][j]=false;
-            }
-        }
+
         if(originx == tx && originy == ty && traverse >= 8)
         {
             PlayerWon(tc);
@@ -210,5 +271,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         return false;
 
-    }
+    }*/
 }
